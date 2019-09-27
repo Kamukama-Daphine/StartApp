5c5c0436160a14ec7926f31982f15cf5e4d8dec0
Kamukama-Daphine <kamdmugisha@gmail.com>
2019-09-27 21:21:29 +0300

    First commit

---
+++ app/src/main/java/ug/ac/must/bcs/startapp/MainActivity.java
@@ -0,0 +1,36 @@
+package ug.ac.must.bcs.startapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settings:
+                startActivity(new Intent(MainActivity.this,Settings.class));
+                default:
+                    break;
+        }
+        return true;
+    }
+}
